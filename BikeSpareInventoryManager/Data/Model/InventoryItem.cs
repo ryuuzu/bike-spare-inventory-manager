7ee0c1cb721f24c6e8ad281b3cbe2efb5d30fb4f
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-04-27 17:52:10 +0545

    Wow

--- BikeSpareInventoryManager/Data/Model/InventoryItem.cs
+++ BikeSpareInventoryManager/Data/Model/InventoryItem.cs
@@ -4,6 +4,20 @@ namespace BikeSpareInventoryManager.Data.Model
 {
     public class InventoryItem
     {
+        /*
+         * Inventory Item class for the items to be stored in the inventory object.
+         * Also helps perform item specific tasks in the inventory.
+         * 
+         * _lastUpdated: Last Update Time of the Item
+         * _createdAt: Creation TIme of the Item
+         * Guid: ID for the item.
+         * Name: Name of the item
+         * Description: Description of the item
+         * Price: Price of the item
+         * Quantity: QUantity of the item in the inventory.
+         * CreatedAt: Accessor and Mutator for the _createdAt.
+         * LastUpdated: Accessor the the _lastUpdated.
+         */
         private DateTime _lastUpdated;
         private DateTime _createdAt;
         public Guid Guid { get; set; } = Guid.NewGuid();
@@ -29,11 +43,13 @@ namespace BikeSpareInventoryManager.Data.Model
             get { return _lastUpdated; }
         }
 
+        // Method to update the LastUpdated to current time.
         public void UpdateNow()
         {
             _lastUpdated = DateTime.Now;
         }
 
+        // Method to decrease the stock of the item as per the request.
         public void DecreaseQuantity(int ToDecrease)
         {
             if (Quantity == 0)
@@ -45,11 +61,6 @@ namespace BikeSpareInventoryManager.Data.Model
                 throw new LowItemStock("Item stock is lower than amount requested.");
             }
             Quantity -= ToDecrease;
-        }
-
-        public static implicit operator Guid(InventoryItem v)
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- BikeSpareInventoryManager/Data/Model/LogsManager.cs
+++ BikeSpareInventoryManager/Data/Model/LogsManager.cs
@@ -5,21 +5,39 @@ namespace BikeSpareInventoryManager.Data.Model
 {
     public class LogsManager
     {
+        /*
+         * Log manager class to create, update, delete the logs.
+         * Also to perform other log specific tasks.
+         * 
+         * _inventoryLogs: All the logs from the system.
+         * InventoryLogs: Accessor the the logs.
+        */
         private List<InventoryLog> _inventoryLogs;
 
         public List<InventoryLog> InventoryLogs { get { return _inventoryLogs; } }
 
+        // Method to get a count of all the logs pending (i.e. Not Approved by and admin)
         public int PendingLogsCount { get { return _inventoryLogs.Where(invLog => invLog.LogType == InvLogType.Withdraw && !invLog.IsApproved).ToList().Count; } }
 
+        // Constructor to create logs manager from the list of logs
         public LogsManager(List<InventoryLog> inventoryLogs)
         {
             _inventoryLogs = inventoryLogs;
         }
 
+        // Constructor to create logs manager from JSON file.
+        public LogsManager(string InventoryLogsJSON)
+        {
+            _inventoryLogs = JsonSerializer.Deserialize<List<InventoryLog>>(InventoryLogsJSON);
+        }
+
+        // Method to get data to be saved into the JSON file.
         public string GetSaveJSON()
         {
             return JsonSerializer.Serialize(InventoryLogs);
         }
+
+        // Method to get the last updated date of an item based on the newest logs.
         public DateTime GetLastUpdatedDate(Guid itemId)
         {
             List<InventoryLog> itemLogs = _inventoryLogs.Where(invLog => invLog.ItemId == itemId).ToList();
@@ -29,20 +47,29 @@ namespace BikeSpareInventoryManager.Data.Model
             }
             else
             {
-                return itemLogs.Aggregate((i1, i2) => i1.ApprovedAt > i2.ApprovedAt ? i1 : i2).ApprovedAt;
+                DateTime lastAddedDate = itemLogs.Aggregate((i1, i2) => i1.LogDate > i2.LogDate ? i1 : i2).LogDate;
+                DateTime lastWithdrawDate = itemLogs.Aggregate((i1, i2) => i1.ApprovedAt > i2.ApprovedAt ? i1 : i2).ApprovedAt;
+                if (lastAddedDate > lastWithdrawDate)
+                {
+                    return lastAddedDate;
+                }
+                return lastWithdrawDate;
             }
         }
 
+        //Method to get item id that belongs to the log
         public Guid GetItemIdFromLog(Guid logId)
         {
             return _inventoryLogs.Find(inventoryLog => inventoryLog.Guid == logId).Guid;
         }
 
+        //Method to get the inventory log based on the id
         public InventoryLog GetInventoryLog(Guid logId)
         {
             return _inventoryLogs.Find(inventoryLog => inventoryLog.Guid == logId);
         }
 
+        //Method to approve the log.
         public void ApproveLog(Guid logGuid, User approver)
         {
             int invLogIndex = _inventoryLogs.FindIndex(inventoryLog => inventoryLog.Guid == logGuid);
@@ -59,6 +86,7 @@ namespace BikeSpareInventoryManager.Data.Model
             _inventoryLogs[invLogIndex].ApprovedAt = DateTime.Now;
         }
 
+        //Method to remove the approval from a log
         public void RemoveApproval(Guid logGuid)
         {
             int invLogIndex = _inventoryLogs.FindIndex(inventoryLog => inventoryLog.Guid == logGuid);
@@ -67,6 +95,7 @@ namespace BikeSpareInventoryManager.Data.Model
             _inventoryLogs[invLogIndex].ApprovedAt = default;
         }
 
+        //Method to get the total number of items that have been taken out from the system.
         public Dictionary<Guid, int> GetItemsTakenOut()
         {
             Dictionary<Guid, int> itemsTakenOutList = new();
@@ -88,11 +117,6 @@ namespace BikeSpareInventoryManager.Data.Model
             }
 
             return itemsTakenOutList;
-        }
-
-        public LogsManager(string InventoryLogsJSON)
-        {
-            _inventoryLogs = JsonSerializer.Deserialize<List<InventoryLog>>(InventoryLogsJSON);
         }
     }
 }
